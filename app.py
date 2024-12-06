e7b4e7a1b3833b9063a1ab3e4a3027d97b8ff5fc
obify <76449271+obify@users.noreply.github.com>
2024-12-06 22:27:13 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'CICD Pipeline updated code'
+    return 'CICD Pipeline 2024'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
